c91f82321892ed87e0c0b027e540218574ada92f
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-03 11:31:49 +0100

    Initial Commit

---
+++ SideMenuSwiftUI/SideMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUIApp.swift
+//  SideMenuSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 3/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
